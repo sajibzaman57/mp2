cdddadcd44008e6e49c1477dd192de6288275ed2
Sajib Zaman <msajib2@illinois.edu>
2025-10-07 22:38:29 -0500

    Added vitejs plug in

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,9 @@ import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   base: "/mp2/",
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  plugins: [tailwindcss(), reactRouter(), tsconfigPaths(), react()],
 });
